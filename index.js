bdf82192c249e4d182a80248271a5ccae868f710
AnsurM <ansurmehdi@gmail.com>
2020-01-12 16:56:14 +0500

    added Cors package and fixed login wrong details status code to 400

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var body = require('body-parser');
+var express = require("express");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var body = require("body-parser");
 var app = express();
 app.use(body.urlencoded({ extended: false }));
 app.use(body.json());
+app.use(cors());
 var router = express.Router();
-app.use('/', router)
-require("./routes/routes")(app,router);
-mongoose.connect
-("mongodb+srv://Auction:model@auction-ujxcw.mongodb.net/test?retryWrites=true&w=majority",
-{ useUnifiedTopology: true, useNewUrlParser: true })
-let db=mongoose.connection;
-db.once('open', ()=>console.log("connected"));
+app.use("/", router);
+// app.use("/", () => "hello world");
+require("./routes/routes")(app, router);
+mongoose.connect(
+  "mongodb+srv://Auction:model@auction-ujxcw.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+let db = mongoose.connection;
+db.once("open", () => console.log("connected"));
 
 module.exports = app;
