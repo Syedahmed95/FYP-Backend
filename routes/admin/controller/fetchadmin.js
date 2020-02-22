090a0e49d6124bf42b0dbc85e1224dc9893869f9
AnsurM <ansurmehdi@gmail.com>
2020-02-23 03:47:31 +0500

    Removed encrypted password check from admin login controller temporarily

--- routes/admin/controller/fetchadmin.js
+++ routes/admin/controller/fetchadmin.js
@@ -4,13 +4,15 @@ const encrpt = require("crypto");
 const fetchAdmin = async (req, res) => {
   let adminuser = req.body;
   try {
-    const hashpass = encrpt.scryptSync('aes-192-cbc', adminuser.password,24);
+    const hashpass = encrpt.scryptSync("aes-192-cbc", adminuser.password, 24);
     //Signing in checking here of admin
     let data = await admin.find({
       username: adminuser.username,
-      password: hashpass
+      // password: hashpass
+      password: adminuser.password
     });
     if (data.length <= 0) {
+      console.log("Admin: ", data);
       res.status(400).json({
         message: "Wrong details"
       });
